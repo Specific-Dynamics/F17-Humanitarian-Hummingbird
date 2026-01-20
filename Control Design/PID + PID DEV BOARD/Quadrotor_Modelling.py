cd1a32767182746294adbc4182cb4d557243d94e
cwlebgarcia <calebf3g4@gmail.com>
2026-01-20 10:43:05 -0700

    Thrust calculations are cooked

--- Control Design/PID + PID DEV BOARD/Quadrotor_Modelling.py
+++ Control Design/PID + PID DEV BOARD/Quadrotor_Modelling.py
@@ -1,19 +1,20 @@
+import math
 
 # Equations and method from Advances in Unmanned Aerial Vehicles: State of the Art and the Road to Autonomy (2007), Chapter 6
 # Other modeling: 
 #   https://www.researchgate.net/publication/343309950_Modeling_Control_and_Simulation_of_Quadrotor_UAV
 #   https://www.sciencedirect.com/science/article/pii/S1367578823000640
 
-# Equations for Modelling a Quadrotor
-
-# How much do we care about
-
-# Section 6.4.1 details 3 important design contstraints:
+# Section 6.4.1 details 3 important design contstraints: lwk I don't think we care
 #   1. Maximum Mass
 #   2. Maximum Span
 #   3. Target Thrust-to-Weight Ratio
 
+##########################################################################################################
+
 # Model Parameters
+
+##########################################################################################################
 
 MAX_MASS: float = 100 # g
 MAX_SPAN: float # mm
@@ -26,14 +27,28 @@ ROTOR_ADVANCE_RATIO: float
 INFLOW_RATIO: float
 INDUCED_VELOCITY: float 
 ROTOR_RADIUS: float
-ROTOR_SPEED: float
+ROTOR_SPEED: float = 3e4 # RPM (Approximate)
 ROTOR_AREA: float
 PITCH_OF_INCIDENCE: float
 TWIST_PITCH: float
 AVERAGE_DRAG_COEFFICIENT: float
 
-# Equations
+##########################################################################################################
 
+# Equations for Modelling a Quadrotor
+
+##########################################################################################################
+
+# Solidity ratio is the ratio of area of the blades to the total area of the roto
+def solidity_ratio(rotor_radius: float, avg_chord: float, num_blades: int) -> float:
+    return num_blades * avg_chord  / (math.pi * rotor_radius)
+
+def pitch_to_twist(pitch: float, rotor_radius: float) -> float:
+    circumference = 2 * rotor_radius * math.pi
+    twist_angle = math.atan(pitch / circumference)
+    return twist_angle
+
+# For simplification, thrust coefficient is set at 0.1
 def thrust_force(
         thrust_coefficient: float,
         air_density: float,
@@ -44,18 +59,45 @@ def thrust_force(
     thrust = thrust_coefficient * air_density * rotor_area * (rotor_speed * rotor_radius) ** 2
     return thrust
 
-def thrust_coefficient(
-        solidity_ratio: float,
-        lift_slope: float,
-        rotor_advance_ratio: float,
-        pitch_of_incidence: float,
-        twist_pitch: float,
-        inflow_ratio: float,
-            ) -> float:
-    thrust_coefficient = solidity_ratio * lift_slope * ()
+##########################################################################################################
+
+# Propeller Class
+
+##########################################################################################################
+
+class Propeller():
+    def __init__(self, diameter: float):
+        self.radius = diameter / 2 # mm
+
+##########################################################################################################
+
+# Main
+
+##########################################################################################################
+
 
 def main() -> None:
-    pass
+    # 2512 Hurricane PC 3 Blade 
+    Hurricane = Propeller(diameter=63.5e-3)
+    # GEMFAN D51
+    D51 = Propeller(diameter=50.3e-3)
+    # GEMFAN 2512
+    GEMFAN2512 = Propeller(diameter=63.76e-3)
+
+    propellers = [Hurricane, D51, GEMFAN2512]
+
+    for propeller in propellers:
+        rotor_area = math.pi * (propeller.radius) ** 2
+        thrust = thrust_force(
+            thrust_coefficient=0.1,
+            air_density=AIR_DENSITY,
+            rotor_area=rotor_area,
+            rotor_speed=ROTOR_SPEED,
+            rotor_radius=propeller.radius
+            )
+        total_thrust = thrust * 4
+        print(total_thrust)
+
 
 if __name__ == "__main__":
     main()
